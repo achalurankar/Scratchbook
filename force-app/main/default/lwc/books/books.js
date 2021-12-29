9fa18dea752bf175b5c37d59fe6bcac6db72b981
Achal Urankar <achal_urankar@persistent.com>
2021-12-29 23:19:15 +0530

    dynamic modal added

--- force-app/main/default/lwc/books/books.js
+++ force-app/main/default/lwc/books/books.js
@@ -1,14 +1,16 @@
 import { LightningElement, track, api } from 'lwc';
 
 import getBooks from '@salesforce/apex/scratchbook_cc.getBooks';
+import saveBook from '@salesforce/apex/scratchbook_cc.saveBook';
 import { dispatchEvent } from 'c/utils';
 
 export default class Books extends LightningElement {
 
     @track books;
     @track showModal;
-    idVsBookMap;
+    idVsBookMap; //map to get book object from its id, mapped in getBooks method
     @track selectedBook;
+    fields; //fields required on edit book modal
 
     connectedCallback() {
         this.loadBooks();
@@ -40,13 +42,44 @@ export default class Books extends LightningElement {
     }
 
     handleNewBookClick(){
+        //emptying old values for fields for new book
+        this.fields = this.getFields("", "");
         this.showModal = true;
     }
 
     handleEditBookClick(event){
         let bookId = event.currentTarget.dataset.id;
         this.selectedBook = this.idVsBookMap[`${bookId}`];
-        // console.log('selectedBook ' + JSON.stringify(this.selectedBook));
+        this.fields = this.getFields(this.selectedBook.name, this.selectedBook.description);
+        // console.log('selectedBook ' + JSON.stringify(this.fields));
         this.showModal = true;
+    }
+
+    handleSave(event){
+        let updatedFields = event.detail.updatedFields;
+        // console.log(JSON.stringify(updatedFields));
+        var params = {};
+        params.name = updatedFields[0].value;
+        params.description = updatedFields[1].value;
+        params.id = this.selectedBook ? this.selectedBook.id : '';
+        saveBook({ requestStructure : JSON.stringify(params) })
+            .then(result => {
+                console.log('book saved with id ' + result);
+                this.showModal = false;
+                this.selectedBook = undefined;
+                this.loadBooks();
+            })
+            .catch(error => {
+                console.log(JSON.stringify(error));
+            });
+        
+    }
+
+    //get fields array for modal rendering
+    getFields(name, description) {
+        return [
+            { label : "Book Name", value : name, uniqueName : "BookName", required : true }, 
+            { label : "Description", value : description, uniqueName : "Description", required : false }
+        ];
     }
 } 
--- force-app/main/default/lwc/modal/modal.js
+++ force-app/main/default/lwc/modal/modal.js
@@ -4,10 +4,20 @@ import { dispatchEvent } from 'c/utils';
 export default class Modal extends LightningElement {
     
     @api modalHeader;
-    @api selectedItem;
+    @api fields;
 
-    handleCancelClick() {
+    handleCancelClick(){
         dispatchEvent(this, 'cancel', {});
     }
-    
+
+    handleSaveClick(){
+        let updatedFields = [];
+        this.fields.forEach(element => {
+            let val = this.template.querySelector(`[data-id="${element.uniqueName}"]`).value;
+            let temp = Object.assign({}, element);
+            temp.value = val;
+            updatedFields.push(temp);
+        });
+        dispatchEvent(this, 'save', { updatedFields : updatedFields });
+    }
 }
